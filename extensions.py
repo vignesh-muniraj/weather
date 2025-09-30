45cdc8471c0c0336eed2f2952d606978b12c31c1
vignesh-muniraj <mnvicky04@gmail.com>
2025-09-30 16:49:59 +0530

    After testing

--- extensions.py
+++ extensions.py
@@ -1,14 +1,4 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from flask_cors import CORS
 
-# db = SQLAlchemy()
-# migrate = Migrate()
-
-# def init_extensions(app):
-#     db.init_app(app)
-#     migrate.init_app(app, db)
-#     CORS(app)  # allow all origins for development
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
--- models.py
+++ models.py
@@ -1,14 +1,4 @@
-# from datetime import datetime
-# from extensions import db
 
-# class Search(db.Model):
-#     __tablename__ = "searches"
-#     id = db.Column(db.Integer, primary_key=True)
-#     city_name = db.Column(db.String(255), nullable=False)
-#     searched_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
-
-#     def __repr__(self):
-#         return f"<Search {self.city_name} @ {self.searched_at}>"
 from extensions import db
 from datetime import datetime
 
