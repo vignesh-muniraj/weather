cf8aa2f3fd7ddc008d304673457156e0445a22c5
vignesh-muniraj <mnvicky04@gmail.com>
2025-09-30 08:39:46 +0530

    working

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# class Config:
+#     DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+#     OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 class Config:
-    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+    DEBUG = True
